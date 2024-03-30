fa62c747d3adfb69177eb725b9fb6547eabe42c8
SofiaGoyesP <s.goyesp@uniandes.edu.co>
2024-03-30 11:26:45 -0500

    Main con scroll

--- app/src/main/java/com/example/proyectomhealth/Results.java
+++ app/src/main/java/com/example/proyectomhealth/Results.java
@@ -3,6 +3,7 @@ package com.example.proyectomhealth;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Results extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class Results extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+
+
+
     }
 }
